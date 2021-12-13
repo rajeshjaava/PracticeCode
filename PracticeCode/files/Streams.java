01b4933b58e8e678a945e07e3a9ad0524fcf892d
Rajesh Thokala <rajeshthokala@Rajeshs-MacBook-Air.local>
2021-12-13 19:31:52 +0530

    streams functions

---
+++ PracticeCode/files/Streams.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+public class Streams{
+public static void main(String args[]){
+System.out.println("streams test");
+List<Integer> list=Arrays.asList(1,2,3,4,5,6);
+System.out.println("count is "+list.stream().count());
+System.out.println("count of list is "+list.stream().reduce(0,(subelement,total)->subelement+total));
+System.out.println("values sum :"+list.stream().reduce(0,Integer::sum));
+}
+}
